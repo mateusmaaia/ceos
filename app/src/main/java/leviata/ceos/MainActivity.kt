2e5656748d33180e8242d01164cee4e6fc1bb240
Henrique Oliveira <riikkii@gmail.com>
2018-08-30 16:26:58 -0300

    VAI BRASIL

--- app/src/main/java/leviata/ceos/MainActivity.kt
+++ app/src/main/java/leviata/ceos/MainActivity.kt
@@ -1,12 +1,14 @@
 package leviata.ceos
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import leviata.ceos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
