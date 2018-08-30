0c964c61af19110c91588769f9ae205ef75f3008
mateusmaaia <maieta2122746554@#>
2018-08-30 19:04:57 -0300

    Fixes for layout + versions

--- app/src/main/java/leviata/ceos/Look.kt
+++ app/src/main/java/leviata/ceos/Look.kt
@@ -3,6 +3,8 @@ package leviata.ceos
 class Look {
 
     var color:String = "#ffffff"
+    var backgroundColorScrim = "#861161"
+    var backgroundGradientScrim = "#f94f3a"
 
     constructor()
 }
--- app/src/main/java/leviata/ceos/LookHelper.kt
+++ app/src/main/java/leviata/ceos/LookHelper.kt
@@ -3,10 +3,26 @@ package leviata.ceos
 import android.databinding.BindingAdapter
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.graphics.drawable.GradientDrawable
 import android.widget.EditText
+import android.widget.ImageView
 
 @BindingAdapter("setUnderlineColor")
 fun setUnderlineColor(view: EditText, color: String?) {
     if (color == null) return
     view.background.setColorFilter(Color.parseColor(color), PorterDuff.Mode.SRC_ATOP)
+}
+
+@BindingAdapter(value = ["backgroundColorScrim", "backgroundGradientScrim"], requireAll = false)
+fun setCollapsingScrimColor(layout: ImageView, color: String?, colorGradient: String?) {
+    if (color == null || color.isEmpty() || colorGradient == null || colorGradient.isEmpty()) return
+
+    val colors = intArrayOf(Color.parseColor(color), Color.parseColor(colorGradient))
+    val gd = GradientDrawable(
+            GradientDrawable.Orientation.TOP_BOTTOM, colors)
+
+    gd.cornerRadius = 0f
+    layout.background = gd
+
+    //layout.setContentScrimColor(LookHelper.convertRgbaToArgb(color))
 }
