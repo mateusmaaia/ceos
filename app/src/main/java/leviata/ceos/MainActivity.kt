b711b2a941143343bc263bac662d65c861b5d800
Henrique Oliveira <riikkii@gmail.com>
2018-08-31 12:37:33 -0300

    Android Animate - Fix

--- app/src/main/java/leviata/ceos/MainActivity.kt
+++ app/src/main/java/leviata/ceos/MainActivity.kt
@@ -3,6 +3,9 @@ package leviata.ceos
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import com.bumptech.glide.Glide
 import leviata.ceos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         binding.look = Look()
+        val handler = Handler()
+
+        Glide.with(this)
+                .load(R.drawable.mind)
+                .into(binding.logo)
+
+        binding.btnLogin.setOnClickListener {
+            binding.logo.visibility = View.VISIBLE
+            binding.logo.startAnimation(R.anim.fade_in) {
+                handler.postDelayed({
+                    binding.logo.visibility = View.INVISIBLE
+                }, 2000)
+
+            }
+        }
     }
 }
