c513596255624028e6c96afe1d72bd8af6b11466
carloosamarante <amarante.carlos@hotmail.com>
2018-08-31 01:07:34 -0300

    Change login and register background

--- app/src/main/java/leviata/ceos/Look.kt
+++ app/src/main/java/leviata/ceos/Look.kt
@@ -3,8 +3,8 @@ package leviata.ceos
 class Look {
 
     var color:String = "#ffffff"
-    var backgroundColorScrim = "#c5c5db"
-    var backgroundGradientScrim = "#ededef"
+    var backgroundColorScrim = "#ffffff"
+    var backgroundGradientScrim = "#8484ff"
     var backgroundButtonColorScrim = "#6451f7"
     var backgroundButtonGradientScrim = "#73efbf"
 
