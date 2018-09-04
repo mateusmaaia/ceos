5097b599ff0c1f9d195f79e8ee101f4118bb5fa3
carloosamarante <amarante.carlos@hotmail.com>
2018-09-04 02:11:13 -0300

    Update logo background

--- app/src/main/java/leviata/ceos/SplashActivity.kt
+++ app/src/main/java/leviata/ceos/SplashActivity.kt
@@ -11,17 +11,12 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //hiding title bar of this activity
         window.requestFeature(Window.FEATURE_NO_TITLE)
-        //making this activity full screen
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_splash)
 
-        //4second splash time
         Handler().postDelayed({
-            //start main activity
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            //finish this activity
             finish()
         },4000)
 
