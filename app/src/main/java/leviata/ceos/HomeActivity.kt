5c4a498bb2e0618ce55fefa3b187bd33ee4b15a0
carloosamarante <amarante.carlos@hotmail.com>
2018-09-16 19:52:05 -0300

    Create HomeActivity and home_xml

--- app/src/main/java/leviata/ceos/HomeActivity.kt
+++ app/src/main/java/leviata/ceos/HomeActivity.kt
@@ -1,4 +1,14 @@
 package leviata.ceos
 
-class HomeActivity {
+import android.databinding.DataBindingUtil
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import leviata.ceos.databinding.ActivityHomeBinding
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
+        binding.look = Look()
+    }
 }
