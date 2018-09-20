cf8eadf9362d2fd141ec8756327041dfbfbe4364
carloosamarante <amarante.carlos@hotmail.com>
2018-09-20 02:15:46 -0300

    Create simple top bar

--- app/src/main/java/leviata/ceos/HomeActivity.kt
+++ app/src/main/java/leviata/ceos/HomeActivity.kt
@@ -3,6 +3,8 @@ package leviata.ceos
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import kotlinx.android.synthetic.main.activity_home.*
 import leviata.ceos.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
@@ -10,5 +12,22 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
         binding.look = Look()
+        // Set the toolbar as support action bar
+        setSupportActionBar(toolbar)
+
+        // Now get the support action bar
+        val actionBar = supportActionBar
+
+        // Set toolbar title/app title
+        actionBar!!.title = "Ceos"
+
+
+        // Set action bar elevation
+        actionBar.elevation = 4.0F
+
+        // Display the app icon in action bar/toolbar
+        actionBar.setDisplayShowHomeEnabled(true)
+        actionBar.setLogo(R.mipmap.ic_launcher)
+        actionBar.setDisplayUseLogoEnabled(true)
     }
 }
