defb314f3a11811da2420d07c72255145c753cd6
carloosamarante <amarante.carlos@hotmail.com>
2018-09-23 17:59:06 -0300

    Create menu icon on actionBar

--- app/src/main/java/leviata/ceos/HomeActivity.kt
+++ app/src/main/java/leviata/ceos/HomeActivity.kt
@@ -3,7 +3,12 @@ package leviata.ceos
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
 import leviata.ceos.databinding.ActivityHomeBinding
+import android.content.Intent
+import android.support.v7.app.ActionBar
+import android.view.MenuItem
+
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,4 +16,24 @@ class HomeActivity : AppCompatActivity() {
         val binding = DataBindingUtil.setContentView<ActivityHomeBinding>(this, R.layout.activity_home)
         binding.look = Look()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    /*- On Options Item Selected --------------------------------------------------------- */
+    // One of the toolbar icons was clicked
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here.
+        val id = item.getItemId()
+
+        if (id == R.id.navigationMenu) {
+            val i = Intent(this@HomeActivity, HomeActivity::class.java)
+            startActivity(i)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
