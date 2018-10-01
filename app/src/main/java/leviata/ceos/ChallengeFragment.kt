9094bc33352ef50ee1cdbe95f675db07795d2810
carloosamarante <amarante.carlos@hotmail.com>
2018-10-01 01:30:02 -0300

    Create challenge layout

--- app/src/main/java/leviata/ceos/ChallengeFragment.kt
+++ app/src/main/java/leviata/ceos/ChallengeFragment.kt
@@ -1,18 +1,19 @@
 package leviata.ceos
 
+import android.support.v4.app.Fragment
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import leviata.ceos.databinding.FragmentSummaryFragmentBinding
+import leviata.ceos.databinding.FragmentChallengeBinding
 
-class ChallengeSummaryFragment {
-    lateinit var binding: FragmentBinding
+class ChallengeFragment : Fragment() {
+    lateinit var binding: FragmentChallengeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(
-                inflater, R.layout.fragment_profile, container, false)
+                inflater, R.layout.fragment_challenge, container, false)
 
         return binding.root
     }
